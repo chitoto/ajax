1a501af2133f407416bfe9cc6b7e52763f4f393c
Yumiko Kamiyama <tototina.ruru@gmail.com>
2021-04-01 18:33:57 +0900

    add:model_message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_01_084247) do
+ActiveRecord::Schema.define(version: 2021_04_01_090904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,17 @@ ActiveRecord::Schema.define(version: 2021_04_01_084247) do
     t.index ["sender_id"], name: "index_conversations_on_sender_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.boolean "read", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -60,4 +71,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_084247) do
   end
 
   add_foreign_key "comments", "blogs"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
 end
